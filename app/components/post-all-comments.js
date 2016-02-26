8157bc39b0bbf297117e1e9df60eae739ec6a21f
Jeffrey Ruder and Kassidy Douglas <ruderjt@gmail.com>
2016-02-25 16:42:44 -0800

    Reimplement sorting of comments

--- app/components/post-all-comments.js
+++ app/components/post-all-comments.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  sortProperties: ['timestamp:desc'],
+  sortedComments: Ember.computed.sort('post.comments', 'sortProperties'),
+
   actions: {
     destroyComment(comment) {
       this.sendAction('destroyComment', comment);
